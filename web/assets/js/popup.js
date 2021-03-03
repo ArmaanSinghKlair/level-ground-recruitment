24ec54b44fc6027cbb297c59eac40b44ec0c52f9
AmirS <AmirS@DESKTOP-EH0EHPK>
2021-03-03 12:51:06 -0700

    .

--- web/assets/js/popup.js
+++ web/assets/js/popup.js
@@ -1,28 +1,28 @@
 $(".open-edu").on("click", function () {
-  $(".edu").fadeIn("slow");
-  $(".wrapper").addClass("active");
+    $(".edu").fadeIn("slow");
+    $(".wrapper").addClass("active");
 });
 
 $(".close").on("click", function () {
-  $(".wrapper").removeClass("active");
+    $(".wrapper").removeClass("active");
     $(".wrapper").removeClass("active-popup");
-  $(".edu").fadeOut();
-  $(".work").fadeOut();
-  $(".skill").fadeOut();
+    $(".edu").fadeOut();
+    $(".work").fadeOut();
+    $(".skill").fadeOut();
     $(".remove-acc-alert").fadeOut();
 });
 
 $(".open-work").on("click", function () {
-  $(".work").fadeIn("slow");
-  $(".wrapper").addClass("active");
+    $(".work").fadeIn("slow");
+    $(".wrapper").addClass("active");
 });
 
 $(".open-skill").on("click", function () {
-  $(".skill").fadeIn("slow");
-  $(".wrapper").addClass("active");
+    $(".skill").fadeIn("slow");
+    $(".wrapper").addClass("active");
 });
 
 $(".open-remove").on("click", function () {
-  $(".remove-acc-alert").fadeIn("slow");
-  $(".wrapper").addClass("active-popup");
+    $(".remove-acc-alert").fadeIn("slow");
+    $(".wrapper").addClass("active-popup");
 });
