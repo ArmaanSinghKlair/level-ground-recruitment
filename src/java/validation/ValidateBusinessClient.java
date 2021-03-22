2155883cd77be76e23cb0db88dc67dc455d2de88
Kp4ws <kentpawson123@gmail.com>
2021-03-21 18:12:13 -0600

    Updated ValidateBusinessClient

--- src/java/validation/ValidateBusinessClient.java
+++ src/java/validation/ValidateBusinessClient.java
@@ -11,99 +11,128 @@ import java.util.regex.Pattern;
 import problemdomain.BusinessClient;
 
 /**
- * To use this class, first call the startValidation, do your validation and then call getErrorMap
+ * To use this class, first call the startValidation, do your validation and
+ * then call getErrorMap
+ *
  * @author 839645
  */
 public final class ValidateBusinessClient {
+
     private static ArrayList<String> errList;
-    
+
     /**
      * Resets the hash map to an empty map
      */
-    public static void startValidation(){
+    public static void startValidation() {
         errList = new ArrayList<>();
     }
-    
+
     //Basic business client validation
-    public static ArrayList<String> getErrorMapForAllfields(String username, String password, String firstName, String lastName, String email, String phoneNo){
+    public static ArrayList<String> getErrorMapForAllfields(String username, String password, String firstName, String lastName, String company, String email, String phoneNo) {
         startValidation();
-        put("busClientUsername",validateBusClientUsername(username));
-        put("busClientPassword",validateBusClientPassword(password));
-        put("busClientFirstName",validateBusClientfirstName(firstName));
+        System.out.println("Firstname:" + firstName);
+
+        put("busClientUsername", validateBusClientUsername(username));
+        put("busClientPassword", validateBusClientPassword(password));
+        put("busClientCompany", validateBusClientCompany(company));
+        put("busClientFirstName", validateBusClientfirstName(firstName));
         put("busClientLastName", validateBusClientlastName(lastName));
-        put("busClientEmail",validateBusClientEmail(email));
-        put("busClientPhoneNo",validateBusClientPhoneNo(phoneNo));
+        put("busClientEmail", validateBusClientEmail(email));
+        put("busClientPhoneNo", validateBusClientPhoneNo(phoneNo));
         return getErrorMap();
     }
-    private static void put(String name, String value){
-        if(value != null)
+
+    private static void put(String name, String value) {
+        if (value != null) {
             errList.add(value);
+        }
     }
-    public static String validateBusClientUsername(String username){
-        if(isEmpty(username))
+
+    public static String validateBusClientUsername(String username) {
+        if (isEmpty(username)) {
             return "Username cannot be empty";
-        else if(username.trim().length() > 45)
+        } else if (username.trim().length() > 45) {
             return "Username cannot be more than 45 characters";
-        else 
+        } else {
             return null;
+        }
     }
-    
-    public static String validateBusClientPassword(String password){
-        if(isEmpty(password))
+
+    public static String validateBusClientPassword(String password) {
+        if (isEmpty(password)) {
             return "Password cannot be empty";
-        else if(password.length() > 100)
+        } else if (password.length() > 100) {
             return "Password cannot be more than 100 characters";
-        else 
+        } else {
             return null;
+        }
     }
-    
-    public static String validateBusClientfirstName(String firstName){
-        if(isEmpty(firstName))
+
+    public static String validateBusClientCompany(String company) {
+
+        if (isEmpty(company)) {
+            return "Company cannot be empty";
+        } else if (company.length() > 100) {
+            return "Company cannot be more than 100 characters";
+        } else {
+            return null;
+        }
+    }
+
+    public static String validateBusClientfirstName(String firstName) {
+
+        if (isEmpty(firstName)) {
             return "First name cannot be empty";
-        else if(firstName.length() > 45)
+        } else if (firstName.length() > 45) {
             return "First name cannot be more than 45 characters";
-        else 
+        } else {
             return null;
+        }
     }
-    
-    public static String validateBusClientlastName(String lastName){
-        if(isEmpty(lastName))
+
+    public static String validateBusClientlastName(String lastName) {
+        if (isEmpty(lastName)) {
             return "Last name cannot be empty";
-        else if(lastName.length() > 45)
+        } else if (lastName.length() > 45) {
             return "Last name cannot be more than 45 characters";
-        else 
+        } else {
             return null;
+        }
     }
-    
-    public static String validateBusClientEmail(String email){
-        if(isEmpty(email))
+
+    public static String validateBusClientEmail(String email) {
+        if (isEmpty(email)) {
             return "Email cannot be empty";
-        else if(email.trim().length() > 255)
-            return  "Email cannot be more than 255 characters";
-        else if(!Pattern.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9+_.-]+$", email))
+        } else if (email.trim().length() > 255) {
+            return "Email cannot be more than 255 characters";
+        } else if (!Pattern.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9+_.-]+$", email)) {
             return "Invalid email provided";
-        else
+        } else {
             return null;
+        }
     }
-    
-    public static String validateBusClientPhoneNo(String phoneNo){
-        if(isEmpty(phoneNo))
+
+    public static String validateBusClientPhoneNo(String phoneNo) {
+        if (isEmpty(phoneNo)) {
             return "Phone no cannot be empty";
-        else if(phoneNo.trim().length() > 10)
+        } else if (phoneNo.trim().length() > 10) {
             return "Phone no cannot be more than 10 characters";
-        else if(!Pattern.matches("\\d{10}", phoneNo))
+        } else if (!Pattern.matches("\\d{10}", phoneNo)) {
             return "Only 10 digits allowed in phone number";
-        else 
+        } else {
             return null;
+        }
     }
-    
-    public static ArrayList<String> getErrorMap(){
-        if(errList.isEmpty())
+
+    public static ArrayList<String> getErrorMap() {
+        if (errList.isEmpty()) {
             return null;
-        else
+        } else {
             return errList;
+        }
     }
-    private final static boolean isEmpty(String field){
+
+    private final static boolean isEmpty(String field) {
         return field == null || field.trim().length() == 0;
     }
 }
