1828454e779fba119d19319fa8a0979f825eda18
Amir Soltani <amir.soltaani@outlook.com>
2021-04-19 21:25:24 -0600

    Several improvements and bug fixes on all pages.

--- src/java/database/dummy_data.sql
+++ src/java/database/dummy_data.sql
@@ -11,31 +11,31 @@
 # Candidate
 INSERT INTO qvt0v9the6uowez2.candidate (can_username, can_password, can_description, can_firstName, can_lastName,
                                         can_email, can_phone_no)
-VALUES ('garnet', 'password',
+VALUES ('garnet', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8',
         'Software developer with 5 years of experience in the IT industry. Looking for new Opportunities',
         'Garnet', 'DuBuque', 'predovic.john@jacobi.com', '5644484814');
 
 INSERT INTO qvt0v9the6uowez2.candidate (can_username, can_password, can_description, can_firstName, can_lastName,
                                         can_email, can_phone_no)
-VALUES ('dhaley', 'password',
+VALUES ('dhaley', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8',
         'Independent full stack web designer and web developer. Worked with companies like A, B, and C.',
         'Zackary', 'Berge', 'angelica.pfannerstill@boyer.com', '2010944683');
 
 INSERT INTO qvt0v9the6uowez2.candidate (can_username, can_password, can_description, can_firstName, can_lastName,
                                         can_email, can_phone_no)
-VALUES ('tia05', 'password',
+VALUES ('tia05', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8',
         'Civil engineer with 10 years of experience in the variety of projects. Worked with latest design and analysis software.',
         'Eriberto', 'Haley', 'lindsey.von@yahoo.com', '6765250867');
 
 INSERT INTO qvt0v9the6uowez2.candidate (can_username, can_password, can_description, can_firstName, can_lastName,
                                         can_email, can_phone_no)
-VALUES ('daphne_cassin', 'password',
+VALUES ('daphne_cassin', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8',
         'App developer with 3 years of experience with React and React Native.',
         'Daphne', 'Cassin', 'runolfsdottir.issac@doyle.com', '5004765184');
 
 INSERT INTO qvt0v9the6uowez2.candidate (can_username, can_password, can_description, can_firstName, can_lastName,
                                         can_email, can_phone_no)
-VALUES ('jamey_hammes', 'password',
+VALUES ('jamey_hammes', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8',
         'Junior database administrator with 2 years of experience with relational databases such as Oracle and MySQL, etc.',
         'Jamey', 'Hammes', 'stephania27@hirthe.com', '7057537044');
 
@@ -43,55 +43,55 @@ VALUES ('jamey_hammes', 'password',
 INSERT INTO qvt0v9the6uowez2.business_client (bus_client_username, bus_client_password, bus_client_company,
                                               bus_client_description, bus_client_address, bus_client_email,
                                               bus_client_phone, bus_client_website)
-VALUES ('graham_kovacek', 'password', 'Graham-Kovacek Inc', 'Grow Next-Generation Partnerships',
+VALUES ('graham_kovacek', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'Graham-Kovacek Inc', 'Grow Next-Generation Partnerships',
         '6055 Dicki Fields Trenton, NJ 08650', 'bstroman@rice.info', '7135081285', 'www.pacificwestedge.com');
 
 INSERT INTO qvt0v9the6uowez2.business_client (bus_client_username, bus_client_password, bus_client_company,
                                               bus_client_description, bus_client_address, bus_client_email,
                                               bus_client_phone, bus_client_website)
-VALUES ('kerlukeplc', 'password', 'Kerluke-Hartmann PLC', 'Cross-Group Impactful Encryption',
+VALUES ('kerlukeplc', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'Kerluke-Hartmann PLC', 'Cross-Group Impactful Encryption',
         '43484 Franecki Vista Apt. 067West Jamar, QC X4N3P0', 'adolphus.mertz@balistreri.com', '4692242549',
         'miamibestbuys.com');
 
 INSERT INTO qvt0v9the6uowez2.business_client (bus_client_username, bus_client_password, bus_client_company,
                                               bus_client_description, bus_client_address, bus_client_email,
                                               bus_client_phone, bus_client_website)
-VALUES ('weberllc', 'password', 'Weber LLC Inc', 'Customer-Focused Multimedia Paradigm',
+VALUES ('weberllc', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'Weber LLC Inc', 'Customer-Focused Multimedia Paradigm',
         '99103 Vella Ford Apt. 759Tatyanabury, NB T8L 9N4', 'mswift@hotmail.com', '5199090706', 'CleaningBasics.ca');
 
 INSERT INTO qvt0v9the6uowez2.business_client (bus_client_username, bus_client_password, bus_client_company,
                                               bus_client_description, bus_client_address, bus_client_email,
                                               bus_client_phone, bus_client_website)
-VALUES ('schulist', 'password', 'Schulist, Gusikowski and Keebler and Sons', 'Robust Systematic Synergy',
+VALUES ('schulist', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'Schulist, Gusikowski and Keebler and Sons', 'Robust Systematic Synergy',
         '346 Schuppe RidgeLake Tanner, NU V7Y 8N5', 'leffler.kiana@lemke.biz', '6048571575', 'SearchRoofing.ca');
 
 INSERT INTO qvt0v9the6uowez2.business_client (bus_client_username, bus_client_password, bus_client_company,
                                               bus_client_description, bus_client_address, bus_client_email,
                                               bus_client_phone, bus_client_website)
-VALUES ('walsh', 'password', 'Walsh and Sons', 'Compatible High-Level Portal',
+VALUES ('walsh', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'Walsh and Sons', 'Compatible High-Level Portal',
         '218 Legros ForksNicholashaven, NU V1J2C4', 'dosinski@shanahan.org', '5700426654', 'AccomodationRentals.ca
 ');
 
 # Advisor
 INSERT INTO qvt0v9the6uowez2.advisor (advisor_username, advisor_password, advisor_firstName, advisor_lastName,
                                       advisor_Email)
-VALUES ('genesis_caroll', 'password', 'Genesis', 'Carroll', 'moore.leonie@nicolas.biz');
+VALUES ('genesis_caroll', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'Genesis', 'Carroll', 'moore.leonie@nicolas.biz');
 
 INSERT INTO qvt0v9the6uowez2.advisor (advisor_username, advisor_password, advisor_firstName, advisor_lastName,
                                       advisor_Email)
-VALUES ('yemard', 'password', 'Madyson', 'Ullrich', 'madyson74@haag.biz');
+VALUES ('yemard', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'Madyson', 'Ullrich', 'madyson74@haag.biz');
 
 INSERT INTO qvt0v9the6uowez2.advisor (advisor_username, advisor_password, advisor_firstName, advisor_lastName,
                                       advisor_Email)
-VALUES ('dharber', 'password', 'Andreanne', 'Oberbrunner', 'cary45@gmail.com');
+VALUES ('dharber', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'Andreanne', 'Oberbrunner', 'cary45@gmail.com');
 
 INSERT INTO qvt0v9the6uowez2.advisor (advisor_username, advisor_password, advisor_firstName, advisor_lastName,
                                       advisor_Email)
-VALUES ('esther81', 'password', 'Yasmeen', 'Mosciski', 'melany14@mills.biz');
+VALUES ('esther81', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'Yasmeen', 'Mosciski', 'melany14@mills.biz');
 
 INSERT INTO qvt0v9the6uowez2.advisor (advisor_username, advisor_password, advisor_firstName, advisor_lastName,
                                       advisor_Email)
-VALUES ('brendon10', 'password', 'Elton', 'Dietrich', 'bboyle@yahoo.com');
+VALUES ('brendon10', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'Elton', 'Dietrich', 'bboyle@yahoo.com');
 
 # Candidate Role
 INSERT INTO qvt0v9the6uowez2.candidate_role (candidateID, roleID)
